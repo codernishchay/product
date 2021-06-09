3a92b7e53bc3da69586715473cb937516dfe3b20
codernishchay <nishchay.vns2000@gmail.com>
2021-06-09 11:16:47 +0530

    updated

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"product/httpd/handler"
+	"product/httpd/methods"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	_product := methods.New()
+
 	r := gin.Default()
-	r.GET("/ping", handler.Pingget)
+	// r.GET("/ping", handler.Pingget)
+	r.GET("/produts", handler.ProdutsGetHandler(_product))
+	r.Post("produts", handler.ProductPostHandler(_product))
+
 	r.Run()
 }
